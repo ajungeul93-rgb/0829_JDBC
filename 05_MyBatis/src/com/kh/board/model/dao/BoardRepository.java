0a60d6806fb6ea7a0227e60b9ccb208f95b29889
Baek Jun Geol <ajungeul93@gamil.com>
2025-09-08 17:33:29 +0900

    09/08

---
+++ 05_MyBatis/src/com/kh/board/model/dao/BoardRepository.java
@@ -0,0 +1,20 @@
+package com.kh.board.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.kh.board.model.vo.Board;
+
+public class BoardRepository {
+
+	public List<Board> selectBoardList(SqlSession session) {
+		return session.selectList("boardMapper.selectBoardList");
+	}
+
+	public Board selectBoard(SqlSession session, int boardNo) {
+		//boardNo = 1;
+		return session.selectOne("boardMapper.selectBoard", boardNo);
+	}
+
+}
